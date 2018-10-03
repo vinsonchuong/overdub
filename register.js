10223712c6e378e9aab35656f7537e2e9fbd07b0
Vinson Chuong <vinsonchuong@gmail.com>
2018-10-03 14:14:25 -0700

    fix(register): Remove rest of stack trace code

--- register.js
+++ register.js
@@ -1,19 +1,4 @@
-/* eslint-disable handle-callback-err */
 /* @flow */
-import { sep } from 'path'
 import babelConfig from 'overdub/babel'
 
 require('@babel/register')(babelConfig)
-
-require('trace')
-require('stack-chain').filter.attach((error, frames) => {
-  return frames.filter(callSite => {
-    const name = callSite && callSite.getFileName()
-    return (
-      name &&
-      name.includes(sep) &&
-      !name.startsWith('internal') &&
-      !name.includes(`pirates${sep}lib${sep}index.js`)
-    )
-  })
-})
